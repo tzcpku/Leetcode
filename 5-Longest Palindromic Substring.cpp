566fa3240e7ebedc4cb2ab26472b6753f874551c
tzcpku <36250847+tzcpku@users.noreply.github.com>
2019-10-24 22:45:47 +0800

    Update 5-Longest Palindromic Substring.cpp

--- 5-Longest Palindromic Substring.cpp
+++ 5-Longest Palindromic Substring.cpp
@@ -2,49 +2,49 @@ class Solution {
 public:
     
 //Approach 1: expand from center value
-//     string longestPalindrome(string s) {
-//         if(s.size() < 2) 
-//             return s;
-//         int n = s.size(), maxLen = 0, start = 0;
-//         for(int i = 0; i < n - 1; i++) {
-//             searchPalindrome(s, i, i, start, maxLen);
-//             searchPalindrome(s, i, i + 1, start, maxLen);
-//         }
-//         return s.substr(start, maxLen);
-//     }
+    string longestPalindrome(string s) {
+        if(s.size() < 2) 
+            return s;
+        int n = s.size(), maxLen = 0, start = 0;
+        for(int i = 0; i < n - 1; i++) {
+            searchPalindrome(s, i, i, start, maxLen);
+            searchPalindrome(s, i, i + 1, start, maxLen);
+        }
+        return s.substr(start, maxLen);
+    }
     
-//     void searchPalindrome(string s, int left, int right, int& start, int& maxLen) {
-//         while(left >= 0 && right < s.size() && s[left] == s[right]) {
-//             --left;
-//             ++right;
-//         }
-//         if(maxLen < right - left - 1) {
-//             start = left + 1;
-//             maxLen = right - left - 1;
-//         }   
-//     }
+    void searchPalindrome(string s, int left, int right, int& start, int& maxLen) {
+        while(left >= 0 && right < s.size() && s[left] == s[right]) {
+            --left;
+            ++right;
+        }
+        if(maxLen < right - left - 1) {
+            start = left + 1;
+            maxLen = right - left - 1;
+        }   
+    }
     
 //Aproach 2: 
-    // string longestPalindrome(string s) {
-    //     if(s.size() < 2) return s;
-    //     int n = s.size(), maxLen = 0, start = 0;
-    //     for(int i = 0; i < n; ) {
-    //         if(n - i <= maxLen / 2) break;
-    //         int left = i, right = i;
-    //         while(right < n - 1 && s[right + 1] == s[right])
-    //             right++;
-    //         i = right + 1;
-    //         while(right < n - 1 && left > 0 && s[right + 1] == s[left - 1]) {
-    //             right++;
-    //             left--;
-    //         }
-    //         if(maxLen < right - left + 1) {
-    //             maxLen = right - left + 1;
-    //             start = left;
-    //         }
-    //     }
-    //     return s.substr(start, maxLen);
-    // }
+    string longestPalindrome(string s) {
+        if(s.size() < 2) return s;
+        int n = s.size(), maxLen = 0, start = 0;
+        for(int i = 0; i < n; ) {
+            if(n - i <= maxLen / 2) break;
+            int left = i, right = i;
+            while(right < n - 1 && s[right + 1] == s[right])
+                right++;
+            i = right + 1;
+            while(right < n - 1 && left > 0 && s[right + 1] == s[left - 1]) {
+                right++;
+                left--;
+            }
+            if(maxLen < right - left + 1) {
+                maxLen = right - left + 1;
+                start = left;
+            }
+        }
+        return s.substr(start, maxLen);
+    }
 
 //Approach 3: dynamic programing
     string longestPalindrome(string s) {
